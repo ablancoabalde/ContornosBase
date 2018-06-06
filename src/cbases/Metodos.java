19968417e4f8d0531b5c75ba8e77d88dc9361bd9
espec <espec@192.168.0.11>
2018-06-06 23:45:30 +0200

    Modelo Controlador

--- src/cbases/Metodos.java
+++ src/cbases/Metodos.java
@@ -5,15 +5,28 @@ import javax.swing.table.DefaultTableModel;
 
 public class Metodos {
 
+    // Instancio la clase Base de datos 
     BaseDatos bd = new BaseDatos();
     Alumno alumno;
 
+    /**
+     * Recibe un modelo de tabla y lo limpia para poder recargarle nuevos
+     * valores
+     *
+     * @param modelo
+     */
     private void limpiarTable(DefaultTableModel modelo) {
         // Limpia la tabla
         modelo.setColumnCount(0);
         modelo.setRowCount(0);
     }
 
+    /**
+     * Metodo que carga los datos recogidos de la base de datos en la tabla
+     * jTAlumno
+     *
+     * @param modelo
+     */
     public void btnCargar(DefaultTableModel modelo) {
         this.limpiarTable(modelo);
 
@@ -29,21 +42,49 @@ public class Metodos {
 
     }
 
+    /**
+     * Metodo que crea un nuevo alumno con los parametros recogidos y los
+     * exporta a la base de datos
+     *
+     * @param nombre
+     * @param nota
+     */
     public void btnAddAlumno(String nombre, int nota) {
         alumno = new Alumno(0, nombre, nota);
         bd.addAlumno(alumno);
     }
-    
-    public void btnDelete(int ref){
+
+    /**
+     * Metodo que exporta la referencia recogido para su borrado de la base de
+     * datos
+     *
+     * @param ref
+     */
+    public void btnDelete(int ref) {
         bd.deleteAlumno(ref);
     }
-    
-    public void btnUpdate(int ref,String nombre, int nota){
+
+    /**
+     * Metodo que exporta los nuevos parametros a cambiar y busca en la base de
+     * datos a traves de la referencia para cambiar los datos nuevos.
+     *
+     * @param ref
+     * @param nombre
+     * @param nota
+     */
+    public void btnUpdate(int ref, String nombre, int nota) {
         bd.updateAlumno(ref, nombre, nota);
     }
-    
-    public void btnSearch(DefaultTableModel modelo2,int ref){
-       this.limpiarTable(modelo2);
+
+    /**
+     * Metodo que carga los datos recogidos de la base de datos, tras realizar
+     * la busqueda de la referencia. En la tabla TablaSearch.
+     *
+     * @param modelo2
+     * @param ref
+     */
+    public void btnSearch(DefaultTableModel modelo2, int ref) {
+        this.limpiarTable(modelo2);
 
         modelo2.addColumn("Referencia");
         modelo2.addColumn("Nombre");
